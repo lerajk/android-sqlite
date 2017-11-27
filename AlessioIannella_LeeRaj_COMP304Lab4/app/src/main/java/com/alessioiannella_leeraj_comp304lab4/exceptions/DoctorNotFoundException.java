e5a31e0217c80a9fdb2790f744a601ac19353002
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 18:44:07 -0500

    // EXCEPTIONS
    Add custom exceptions

---
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/exceptions/DoctorNotFoundException.java
@@ -0,0 +1,12 @@
+package com.alessioiannella_leeraj_comp304lab4.exceptions;
+
+/**
+ * Created by alessio on 27-Nov-17.
+ */
+
+public class DoctorNotFoundException extends Exception{
+
+    public DoctorNotFoundException(String message) {
+        super(message);
+    }
+}

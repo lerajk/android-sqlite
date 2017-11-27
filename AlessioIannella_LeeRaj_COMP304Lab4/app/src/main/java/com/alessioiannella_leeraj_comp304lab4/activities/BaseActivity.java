d17aa382899961df8e944f179fda756d41d88287
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 13:52:43 -0500

    // BASE ACTIVITY
    Created base activity to hold dbhelper singleton

---
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.alessioiannella_leeraj_comp304lab4.activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.alessioiannella_leeraj_comp304lab4.helpers.DBHelper;
+
+public class BaseActivity extends AppCompatActivity {
+
+    protected DBHelper dbHelper;
+}

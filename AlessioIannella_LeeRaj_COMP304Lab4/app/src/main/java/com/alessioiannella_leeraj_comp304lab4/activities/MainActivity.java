6320328d2366ca49e6f93a6bee71a03c2c36743b
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 13:53:30 -0500

    // MAIN ACTIVITY
    Get instance of dbhelper oncreate

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/MainActivity.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/MainActivity.java
@@ -3,11 +3,13 @@ package com.alessioiannella_leeraj_comp304lab4.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
 
 import com.alessioiannella_leeraj_comp304lab4.R;
+import com.alessioiannella_leeraj_comp304lab4.helpers.DBHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     private ProgressBar progressBarLoading;
 
@@ -18,15 +20,12 @@ public class MainActivity extends AppCompatActivity {
 
         progressBarLoading = (ProgressBar) findViewById(R.id.progressBarLoading);
 
-        new android.os.Handler().postDelayed(
-            new Runnable() {
-                public void run() {
-                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                    startActivity(intent);
-                }
-            }, 2000);
+        this.dbHelper = DBHelper.getInstance(this);
 
+        progressBarLoading.setVisibility(View.GONE);
 
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
 
     }
 }

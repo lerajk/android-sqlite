d872999dde2164d59e104a1969ce2b64530607ba
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 14:16:47 -0500

    // REGISTRATION ACTIVITY
    Error validation

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
@@ -7,15 +7,23 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.alessioiannella_leeraj_comp304lab4.R;
+
+import org.w3c.dom.Text;
 
 public class RegistrationActivity extends BaseActivity {
 
     private RadioButton radioButtonNurse;
     private RadioButton radioButtonDoctor;
     private EditText editTextID;
-    private EditText
+    private EditText editTextFirstName;
+    private EditText editTextLastName;
+    private EditText editTextDepartment;
+    private EditText editTextPassword;
+    private EditText editTextConfirmPassword;
+    private TextView textViewErrorRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +34,51 @@ public class RegistrationActivity extends BaseActivity {
     public void handleOnCLikcGoToLogin(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+
+        radioButtonNurse = (RadioButton) findViewById(R.id.radioButtonNurse);
+        radioButtonDoctor = (RadioButton) findViewById(R.id.radioButtonDoctor);
+
+        editTextID = (EditText) findViewById(R.id.editTextID);
+        editTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
+        editTextLastName = (EditText) findViewById(R.id.editTextLastName);
+        editTextDepartment = (EditText) findViewById(R.id.editTextDepartment);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextConfirmPassword = (EditText) findViewById(R.id.editTextConfirmPassword);
+
+        textViewErrorRegistration = (TextView) findViewById(R.id.textViewErrorRegistration);
+
     }
 
     public void handleOnClickRegister(View view){
 
+        if (editTextID.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please enter ID");
+            return;
+        }
+        if (editTextFirstName.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please enter First Name");
+            return;
+        }
+        if (editTextLastName.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please enter Last Name");
+            return;
+        }
+        if (editTextDepartment.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please enter Department");
+            return;
+        }
+        if (editTextPassword.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please enter Password");
+            return;
+        }
+        if (editTextConfirmPassword.getText().toString().isEmpty()){
+            textViewErrorRegistration.setText("Please confirm password");
+            return;
+        }
+        if (!editTextPassword.getText().toString().equals(editTextConfirmPassword.getText().toString())){
+            textViewErrorRegistration.setText("Passwords do not match");
+            return;
+        }
 
-
-        this.dbHelper.getWritableDatabase().execSQL();
     }
 }

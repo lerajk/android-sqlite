3d9b58de7a298b2f7514ef32f74d66c6b7a151cc
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 13:55:49 -0500

    // REGISTRATION ACTIVITY
    Extends Base Activity

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 import com.alessioiannella_leeraj_comp304lab4.R;
 
-public class RegistrationActivity extends AppCompatActivity {
+public class RegistrationActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {

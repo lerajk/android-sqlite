02158d3aa2cb02c5c3df7b6ba1723d0416bfaf48
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 13:58:56 -0500

    // REGISTRATION ACTIVITY
    Button to redirect to login

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
@@ -1,7 +1,9 @@
 package com.alessioiannella_leeraj_comp304lab4.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.alessioiannella_leeraj_comp304lab4.R;
 
@@ -11,5 +13,10 @@ public class RegistrationActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+    }
+
+    public void handleOnCLikcGoToLogin(View view){
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }

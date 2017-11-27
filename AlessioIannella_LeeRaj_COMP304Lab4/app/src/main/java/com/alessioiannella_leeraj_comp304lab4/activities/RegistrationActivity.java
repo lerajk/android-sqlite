35b2b2be0d5e651fa65be0787038ab62546660d0
alessioRM87 <alessio.iannella@gmail.com>
2017-11-27 14:08:37 -0500

    // REGISTRATION ACTIVITY
    Scroll view to include other fields

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/activities/RegistrationActivity.java
@@ -4,10 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 import com.alessioiannella_leeraj_comp304lab4.R;
 
 public class RegistrationActivity extends BaseActivity {
+
+    private RadioButton radioButtonNurse;
+    private RadioButton radioButtonDoctor;
+    private EditText editTextID;
+    private EditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,12 @@ public class RegistrationActivity extends BaseActivity {
     public void handleOnCLikcGoToLogin(View view){
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void handleOnClickRegister(View view){
+
+
+
+        this.dbHelper.getWritableDatabase().execSQL();
     }
 }

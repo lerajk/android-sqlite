ed1e9607f036ed73deadf0aa3d45b4023c8e7b76
alessioRM87 <alessio.iannella@gmail.com>
2017-11-28 15:16:52 -0500

    // MORE EXCEPTIONS
    Added new exceptions

---
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/exceptions/PatientNotFoundException.java
@@ -0,0 +1,12 @@
+package com.alessioiannella_leeraj_comp304lab4.exceptions;
+
+/**
+ * Created by alessio on 28-Nov-17.
+ */
+
+public class PatientNotFoundException extends Exception {
+
+    public PatientNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/exceptions/TestNotFoundException.java
@@ -0,0 +1,12 @@
+package com.alessioiannella_leeraj_comp304lab4.exceptions;
+
+/**
+ * Created by alessio on 28-Nov-17.
+ */
+
+public class TestNotFoundException extends Exception {
+
+    public TestNotFoundException(String message) {
+        super(message);
+    }
+}

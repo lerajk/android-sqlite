7b99e8dd9ab8316349784cb6541f232863804dd4
alessioRM87 <alessio.iannella@gmail.com>
2017-11-28 15:17:21 -0500

    // DB HELPER
    Saved login info in shared preferences, added new methods for test and
    patient

--- AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/helpers/DBHelper.java
+++ AlessioIannella_LeeRaj_COMP304Lab4/app/src/main/java/com/alessioiannella_leeraj_comp304lab4/helpers/DBHelper.java
@@ -2,6 +2,7 @@ package com.alessioiannella_leeraj_comp304lab4.helpers;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -10,8 +11,12 @@ import com.alessioiannella_leeraj_comp304lab4.exceptions.DoctorNotFoundException
 import com.alessioiannella_leeraj_comp304lab4.exceptions.DuplicateIDException;
 import com.alessioiannella_leeraj_comp304lab4.exceptions.LoginFailedException;
 import com.alessioiannella_leeraj_comp304lab4.exceptions.NurseNotFoundException;
+import com.alessioiannella_leeraj_comp304lab4.exceptions.PatientNotFoundException;
+import com.alessioiannella_leeraj_comp304lab4.exceptions.TestNotFoundException;
 import com.alessioiannella_leeraj_comp304lab4.models.Doctor;
 import com.alessioiannella_leeraj_comp304lab4.models.Nurse;
+import com.alessioiannella_leeraj_comp304lab4.models.Patient;
+import com.alessioiannella_leeraj_comp304lab4.models.Test;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -51,7 +56,9 @@ public class DBHelper extends SQLiteOpenHelper {
 
     }
 
-    public void login(String id, String password) throws LoginFailedException{
+    public void login(Context context, String id, String password) throws LoginFailedException{
+
+        boolean isDoctor = false;
 
         sqLiteDatabase = getReadableDatabase();
 
@@ -69,9 +76,18 @@ public class DBHelper extends SQLiteOpenHelper {
             if (cursor == null || cursor.getCount() == 0){
                 throw new LoginFailedException("Wrong Id/password. Please try again");
             }
+
+            isDoctor = true;
         }
 
-        // SAVE LOGIN INFO IN PREFERENCES
+        SharedPreferences sharedPref = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("id", cursor.getString(0));
+        editor.putString("firstName", cursor.getString(1));
+        editor.putString("lastName", cursor.getString(2));
+        editor.putString("department", cursor.getString(3));
+        editor.putBoolean("isDoctor", isDoctor);
+        editor.commit();
     }
 
     public Nurse getNurse(String nurseID) throws NurseNotFoundException {
@@ -124,7 +140,7 @@ public class DBHelper extends SQLiteOpenHelper {
         return doctor;
     }
 
-    public void addNurse(Nurse nurse) throws DuplicateIDException{
+    public void addNurse(Context context, Nurse nurse) throws DuplicateIDException{
 
         sqLiteDatabase = getReadableDatabase();
 
@@ -148,9 +164,18 @@ public class DBHelper extends SQLiteOpenHelper {
 
         sqLiteDatabase.insert("Nurse", null, values);
         sqLiteDatabase.close();
+
+        SharedPreferences sharedPref = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("id", cursor.getString(0));
+        editor.putString("firstName", cursor.getString(1));
+        editor.putString("lastName", cursor.getString(2));
+        editor.putString("department", cursor.getString(3));
+        editor.putBoolean("isDoctor", false);
+        editor.commit();
     }
 
-    public void addDoctor(Doctor doctor) throws DuplicateIDException{
+    public void addDoctor(Context context, Doctor doctor) throws DuplicateIDException{
 
         sqLiteDatabase = getReadableDatabase();
 
@@ -173,6 +198,117 @@ public class DBHelper extends SQLiteOpenHelper {
         values.put("password", doctor.getPassword());
 
         sqLiteDatabase.insert("Doctor", null, values);
+        sqLiteDatabase.close();
+
+        SharedPreferences sharedPref = context.getSharedPreferences("login", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putString("id", cursor.getString(0));
+        editor.putString("firstName", cursor.getString(1));
+        editor.putString("lastName", cursor.getString(2));
+        editor.putString("department", cursor.getString(3));
+        editor.putBoolean("isDoctor", true);
+        editor.commit();
+    }
+
+    public Test getTestByID(String id) throws TestNotFoundException{
+        sqLiteDatabase = getReadableDatabase();
+
+        String[] columns = new String[]{ "testID", "patientID", "nurseID", "bpl", "bph", "temperature" };
+        String[] parameters = new String[]{ id };
+
+        Cursor cursor = sqLiteDatabase.query("Test", columns, "testID=?", parameters, null, null, null, null);
+
+        if (cursor == null || cursor.getCount() == 0){
+            throw new TestNotFoundException("Test with ID " + id + " not found");
+        }
+
+        cursor.moveToFirst();
+
+        Test test = new Test();
+        test.setTestID(cursor.getString(0));
+        test.setPatientID(cursor.getString(1));
+        test.setNurseID(cursor.getString(2));
+        test.setBpl(cursor.getDouble(3));
+        test.setBph(cursor.getDouble(4));
+        test.setTemperature(cursor.getDouble(5));
+
+        return test;
+    }
+
+    public void addTest(Test test) throws DuplicateIDException{
+        sqLiteDatabase = getReadableDatabase();
+
+        String[] columns = new String[]{ "testID" };
+        String[] parameters = new String[]{ test.getTestID() };
+
+        Cursor cursor = sqLiteDatabase.query("Test", columns, "testID=?", parameters, null, null, null, null);
+
+        if (cursor != null && cursor.getCount() > 0){
+            throw new DuplicateIDException("ID already exists!");
+        }
+
+        sqLiteDatabase = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("testID", test.getTestID());
+        values.put("patientID", test.getPatientID());
+        values.put("nurseID", test.getNurseID());
+        values.put("bpl", test.getBpl());
+        values.put("bph", test.getBph());
+        values.put("temperature", test.getTemperature());
+
+        sqLiteDatabase.insert("Test", null, values);
+        sqLiteDatabase.close();
+    }
+
+    public Patient getPatientByID(String id) throws PatientNotFoundException{
+        sqLiteDatabase = getReadableDatabase();
+
+        String[] columns = new String[]{ "patientID", "firstName", "lastName", "department", "doctorID", "room" };
+        String[] parameters = new String[]{ id };
+
+        Cursor cursor = sqLiteDatabase.query("Patient", columns, "patientID=?", parameters, null, null, null, null);
+
+        if (cursor == null || cursor.getCount() == 0){
+            throw new PatientNotFoundException("Patient with ID " + id + " not found");
+        }
+
+        cursor.moveToFirst();
+
+        Patient patient = new Patient();
+        patient.setPatientID(cursor.getString(0));
+        patient.setFirstName(cursor.getString(1));
+        patient.setLastName(cursor.getString(2));
+        patient.setDepartment(cursor.getString(3));
+        patient.setDoctorID(cursor.getString(4));
+        patient.setRoom(cursor.getString(5));
+
+        return patient;
+    }
+
+    public void addPatient(Patient patient) throws DuplicateIDException{
+        sqLiteDatabase = getReadableDatabase();
+
+        String[] columns = new String[]{ "patientID" };
+        String[] parameters = new String[]{ patient.getPatientID() };
+
+        Cursor cursor = sqLiteDatabase.query("Patient", columns, "patientID=?", parameters, null, null, null, null);
+
+        if (cursor != null && cursor.getCount() > 0){
+            throw new DuplicateIDException("ID already exists!");
+        }
+
+        sqLiteDatabase = getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put("patientID", patient.getPatientID());
+        values.put("firstName", patient.getFirstName());
+        values.put("lastName", patient.getLastName());
+        values.put("department", patient.getDepartment());
+        values.put("doctorID", patient.getDoctorID());
+        values.put("room", patient.getRoom());
+
+        sqLiteDatabase.insert("Patient", null, values);
         sqLiteDatabase.close();
     }
 }
